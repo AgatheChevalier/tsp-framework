b0f7a85d50b0a3c8f80d0f4b4da29f7fc9a37d23
aciri <aciri@172.16.125.192>
2018-10-24 12:11:37 +0200

    Avancement de la classe fourmis

--- src/tsp/Fourmi.java
+++ src/tsp/Fourmi.java
@@ -1,21 +1,30 @@
 package tsp;
 
-public class Fourmi {
+public class Fourmi extends Pheromones{
 	
-	private int pos_depart;		//position initiale de la fourmi k
-	private int[] memoire;		//liste des villes visitées par la fourmi
-	private double[][] qte_pheromone;	//quantité de phéromones laissé par la fourmi sur chaque arc
+	private int pos_depart;		
+	//position initiale de la fourmi k
+	private int position; 		
+	//position à l'instant t
+	private int[] memoire;		
+	//liste des villes visitées par la fourmi
+	private double[][] qte_pheromone;	
+	//quantité de phéromones laissé par la fourmi sur chaque arc
+	private double[][] qte_init;
 	private Instance m_instance;
 	
-	public Fourmi(Instance instance, int position_init, int[] villes, double[][] pheromones) {
+	
+	public Fourmi(Instance instance, int position_init, int position, int[] villes, double[][] pheromones,double a, double b, double p, double Lk, double[][] qte_pheromone, double[][] qte_init) {
+		super(a,b,p,Lk,qte_pheromone,qte_init);
 		this.m_instance = instance;
 		this.pos_depart = position_init;
+		this.position = position;
 		this.memoire = villes;
 		this.qte_pheromone = pheromones;
 	}
 	
-	public long distance (int ville_1, int ville_2) throws Exception {	//distance entre 2 villes
-		return  m_instance.getDistances(ville_1, ville_2);
+	public long distance (int i, int j) throws Exception {	//distance entre 2 villes
+		return  m_instance.getDistances(i, j);
 	}
 	
 	public long distance_totale (int position_init) throws Exception {
@@ -32,11 +41,27 @@ public class Fourmi {
 	}
 	
 	public void depot_pheromones() {
-		
+		//à remplir
+	}
+	
+	public double probabilite(int i, int j) {
+		double p = 0;
+		if (this.position == this.memoire[this.memoire.length-1]) {
+			//si la fourmi a visité toutes les villes sauf la ville de départ :
+			p = 1;
+		}
+		else if (this.position == this.memoire[this.memoire.length]) { 
+			//si la fourmi a fait un tour complet :
+			p=0;
+		} else {
+			
+		}
+		return p;
 	}
 	
 	public void choixDeVilleSuivante () {
-		// probabilité de choisir une ville proportionnelle à la quantité de phéromones présents sur le chemin qui mène à cette ville
+		// probabilité de choisir une ville proportionnelle à 
+		//la quantité de phéromones présents sur le chemin qui mène à cette ville
 	}
 	
 	public double visibilite (int villeA, int villeB) throws Exception {
