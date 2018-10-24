17011dbf3affdd41bca1a93d98f1fd808e2a0777
aciri <aciri@172.16.125.192>
2018-10-24 08:42:53 +0200

    Création de classe fourmi + quelques méthodes relatives à cette classe

---
+++ src/tsp/Fourmi.java
@@ -0,0 +1,38 @@
+package tsp;
+
+public class Fourmi {
+	
+	private int pos_depart;		//position initiale de la fourmi k
+	private int[] memoire;		//liste des villes visitées par la fourmi
+	private double[][] qte_pheromone;	//quantité de phéromones laissé par la fourmi sur chaque arc
+	private Instance m_instance;
+	
+	public Fourmi(Instance instance, int position_init, int[] villes, double[][] pheromones) {
+		this.m_instance = instance;
+		this.pos_depart = position_init;
+		this.memoire = villes;
+		this.qte_pheromone = pheromones;
+	}
+	
+	public long distance (int ville_1, int ville_2) throws Exception {	//distance entre 2 villes
+		return  m_instance.getDistances(ville_1, ville_2);
+	}
+	
+	public long distance_totale (int position_init) throws Exception {
+		int i = 0;
+		long d_tot = 0;
+		while (i<this.memoire.length) {
+			d_tot += d_tot + distance(memoire[i], memoire[i+1]);
+			i++;
+		}
+		if (i==this.memoire.length) {
+			d_tot += d_tot + distance(memoire[i], memoire[0]);
+		}
+		return d_tot;
+	}
+	
+	public void depot_pheromones() {
+		
+	}
+
+}
